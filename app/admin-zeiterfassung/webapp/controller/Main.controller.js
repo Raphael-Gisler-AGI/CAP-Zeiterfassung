928e2561fd55057d5bec2d8f061a0e8b8ca75a01
Raphael Gisler <raphael.gisler@agilita.ch>
2024-02-20 09:35:20 +0100

    Remove bloat from admin

--- app/admin-zeiterfassung/webapp/controller/Main.controller.js
+++ app/admin-zeiterfassung/webapp/controller/Main.controller.js
@@ -1,18 +1,5 @@
 sap.ui.define(["./BaseController"], function (BaseController) {
   "use strict";
 
-  return BaseController.extend("adminzeiterfassung.controller.Main", {
-    async onPressCreateProject() {
-      const result = this.getModel().bindList("/Categories").create();
-      this.getView().setBusy(true);
-      await result.created();
-      this.getView().setBusy(false);
-      this.navToManageProject(result.getProperty("ID"));
-    },
-    onNavProject(oEvent) {
-      this.getRouter().navTo("RouteProject", {
-        id: oEvent.getSource().getBindingContext().getProperty("ID"),
-      });
-    },
-  });
+  return BaseController.extend("adminzeiterfassung.controller.Main", {});
 });
